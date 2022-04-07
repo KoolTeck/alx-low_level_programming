464b599b8cd46db6acf91425053c14d941a33af7
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-07 14:57:06 -0700

    added the hello puts program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+
+/**
+ *main: entry
+ *
+ *Return: Always 0 (Succes)
+*/
+
+int main(void)
+{
+  char str[] = "\"Programming is like building a multilingual puzzle";
+  puts(str);
+  return 0;
+}
