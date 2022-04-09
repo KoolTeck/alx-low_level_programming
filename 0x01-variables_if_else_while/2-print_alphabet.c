11a97b1c10596b01c4d0a90cc18a6c3290ad9c53
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-08 18:14:10 -0700

    added prohram to print lower case alpa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+*/
+int main(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+}
