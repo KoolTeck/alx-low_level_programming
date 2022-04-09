b40e3577a778a7cd061c677acaf3e20595b3e37a
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-09 05:13:28 -0700

    added program to print hexadecimal digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+char n = '0';
+char a = 'a';
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+while (a <= 'f')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+
+return (0);
+}
