73f5576542a0b3cab3ba0afd03fcc4930e859fd2
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-09 15:15:44 -0700

    added program to print all combo of 3digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+*/
+int main(void)
+{
+int x, y, z;
+
+x = 48;
+y = 48;
+z = 48;
+while (x < 58)
+{
+y = x + 1;
+z = y + 1;
+while (y < 58)
+{
+while (z < 58)
+{
+putchar(x);
+putchar(y);
+putchar(z);
+if (x < 55 || y < 56 || z < 57)
+{
+putchar(44);
+putchar(20);
+}
+z++;
+}
+y++;
+}
+x++;
+}
+return (0);
+}
