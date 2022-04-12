26fce19bd543d05a85601978616dfe7b5066bf43
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-12 15:29:40 -0700

    added function to ceck lower case char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: c is an ascii character
+ *
+ * Return: 1 on succes
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
