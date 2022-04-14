09ac3085273121d92d1a98e86210f66e19cb000d
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-14 02:34:53 -0700

    added function to print up to 9 digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 - 9
+ *
+ * Return: Always void.
+ */
+void print_numbers(void)
+{
+for (int i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
