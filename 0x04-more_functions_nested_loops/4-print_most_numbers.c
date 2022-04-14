634faeec65c234eab8d17cf006c6a39de8b89fdc
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-14 10:40:22 -0700

    update 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_most_numbers(void)
 {
-for (int i = '0'; i <= '9'; i++)
+int c;
+
+for (c = '0'; c <= '9'; c++)
 {
-if (i != '2' && i != '4')
-{
-_putchar(i);
-}
+if (c != '2' && c != '4')
+_putchar(c);
 }
 _putchar('\n');
 }
