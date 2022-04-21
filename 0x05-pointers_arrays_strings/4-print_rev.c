872c64b4e829726337137e4482c15bb871607eb3
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-21 05:06:11 -0700

    update printrev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,17 +11,6 @@ void print_rev(char *s)
 {
 int i = 0;
 int len;
-if (s[0] != 0)
-{
-while (s[i] != 0)
-{
-len = i;
-i++;
-}
-void print_rev(char *s)
-{
-int i = 0;
-int len;
 int j = 0;
 if (s[0] != 0)
 {
@@ -35,13 +24,6 @@ while (len + 1  > j)
 {
 _putchar(s[len]);
 len--;
-}
-_putchar('\n');
-}
-else
-{
-write(1, "\n", 1);
-}
 }
 _putchar('\n');
 }
