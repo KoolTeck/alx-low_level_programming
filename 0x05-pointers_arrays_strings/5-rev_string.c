465c97dfee321add52442e62b17ef9379cae361f
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-21 10:15:52 -0700

    added sring rev funct

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: pointer to the char array.
+ *
+ * Return: nothing.
+ */
+void rev_string(char *s)
+{
+int len;
+int i, j, k, tmp;
+
+i = 0;
+k = 0;
+while (k < s[k])
+{
+len = k;
+k++;
+}
+j = len;
+while (i < j)
+{
+tmp = s[i];
+s[i] = s[j];
+s[j] = tmp;
+i++;
+j--;
+}
+}
