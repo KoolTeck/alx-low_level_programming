df762ab450e2aefb96a2d757a10fd05d15bb983a
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-20 21:02:29 -0700

    added function with pointer param

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of a pointer to 98.
+ *
+ * @n: pointer to the int.
+ *
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# POINTERS AND ARRAY
+
+This directory contains solution to pointers, array and string taks.
