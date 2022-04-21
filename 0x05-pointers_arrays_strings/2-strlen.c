0aaabf696bbf9a1d67acc8662bc94a8942126f59
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-20 22:14:15 -0700

    added the function strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: pointer to chars to check
+ *
+ * Return: int always true.
+ */
+
+int _strlen(char *s)
+{
+int len;
+for (int i = 0; i < s[i]; i++)
+{
+len = i;
+}
+
+return (len + 1);
+}
