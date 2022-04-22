b5a92bbf6dfa9d961661275fba7ca6eac957c12f
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-21 17:20:51 -0700

    added function to print n array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints an array of n elements.
+ *
+ * @a: the pointer to the array
+ * @n: the size of the array to print.
+ *
+ * Return: Always empty.
+ */
+void print_array(int *a, int n)
+{
+int i = 0, tmp;
+tmp = *&a[n - 1];
+while (i < n)
+{
+printf("%d", *&a[i]);
+if (*&a[i] != tmp)
+{
+printf(", ");
+}
+i++;
+}
+_putchar('\n');
+}
