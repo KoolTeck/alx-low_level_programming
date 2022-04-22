79e37a23152711825aeeaa289b67c038a9d9dd56
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-22 16:56:26 -0700

    added stncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - concats two strings
+ *
+ *@dest: pointer to the fisrt str.
+ *@src: pointer to the second str.
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j, len;
+
+i = 0;
+j = 0;
+if (src[i] != 0)
+{
+while (dest[i] != 0)
+{
+len = i;
+i++;
+}
+len++;
+while (j < n && src[j] != '\0')
+{
+dest[len + j] = src[j];
+j++;
+}
+}
+return (dest);
+}
