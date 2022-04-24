9ef51e51b22c2b40de3fa5ab8a18aaaf7e9db16f
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-24 08:12:20 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,6 +12,8 @@ int _strcmp(char *s1, char *s2)
 {
 int i, result;
 i = 0;
+if (s1[i] != 0 && s2[i != 0])
+{
 while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] > s2[i])
@@ -29,8 +31,12 @@ if (s1[i] == s2[i])
 result = 0;
 break;
 }
-printf("%c %c\n", s1[i], s2[i]);
 i++;
+}
+}
+else
+{
+result = s1[i] || s2[i];
 }
 return (result);
 }
