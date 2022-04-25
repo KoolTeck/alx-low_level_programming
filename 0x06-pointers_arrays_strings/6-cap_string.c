86181423a4a58508e189682df73c7824539d95c7
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-24 18:44:01 -0700

    added word cap funct

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize words of string.
+ *
+ * @s: the pointer to the string;
+ *
+ * Return: A pointer to the cap str.
+ */
+char *cap_string(char *s)
+{
+int i, j, tmp;
+
+int sep[] = {9, 10, 32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; sep[j] != '\0'; j++)
+{
+if (s[i] == sep[j])
+{
+tmp = i;
+tmp++;
+if (s[tmp] >= 'a' && s[tmp] <= 'z')
+{
+s[tmp] = s[tmp] - 32;
+}
+}
+}
+}
+return (s);
+}
