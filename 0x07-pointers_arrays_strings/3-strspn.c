5208d8df6ff3da2a37ecff44b35a80cb04a4cebc
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-27 13:47:32 -0700

    updates

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strspn - gets the lenght of a substring.
  *
  * @s: the string to check
- * @accept:source str.
+ * @accept: source str.
  *
  * Return: number of bytes in the initial segment of s;
  */
 unsigned int _strspn(char *s, char *accept)
 {
 int i, j;
-unsigned int n, prev;
+unsigned int n = 0;
+unsigned int prev = 0;
 
-n = 0;
-prev = 0;
+
 for (i = 0; s[i] != '\0'; i++)
 {
 prev = n;
-for (j = 0; accept[j] != '\0'; j++)
+ for (j = 0; *(accept + j) != '\0'; j++)
 {
-if (accept[j] == s[i])
+if (*(accept + j) == s[i])
 {
 n++;
 }
