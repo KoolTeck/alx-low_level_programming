eefe82c01a1a36a76980538c1982efbd3080b194
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-28 07:52:51 -0700

    added strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring in a string
+ *
+ * @haystack: the string to check
+ * @needle: the substr to check
+ *
+ * Return: a pointer to beginnin of needle.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i, j, len, track;
+
+len = 0;
+while (needle[len] != '\0')
+{
+len++;
+}
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; j < len && haystack[i] == needle[j]; j++, i++)
+{
+if (j == 0)
+{
+track = i;
+}
+if (j == len - 1)
+{
+return (haystack + track);
+}
+}
+}
+return (NULL);
+}
