a919a1b96208981cacca5440825e6b582be52472
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-30 14:26:33 -0700

    updates

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,13 +6,11 @@
  * @n: the int to check
  * @odd: odd num to compare with;
  * @count: natural num to add up to even num.
-7 *
+ *
  * Return: the sqrt of n if found, -1 if not found.
  */
 int check(int n, int odd, int count)
 {
-int sum;
-long  _sqrt;
 if (n < 0)
 {
 return (-1);
@@ -22,13 +20,7 @@ if (n == 0)
 return (0);
 }
 count += 1;
-sum = check(n - odd, (2L * count) -1, count);
-if (sum < 0)
-{
-return (-1);
-}
-_sqrt = (check(n - odd, 2L * count - 1, count) + 1);
-return (_sqrt);
+return (check(n - odd, 2 * count - 1, count) + 1);
 }
 /**
  * _sqrt_recursion - returns the sqrt of n.
