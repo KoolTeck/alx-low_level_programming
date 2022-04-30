302643d7809856d327507b8fe29ca41e3c30f048
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-04-30 14:13:42 -0700

    updates

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,9 +9,10 @@
 7 *
  * Return: the sqrt of n if found, -1 if not found.
  */
-long int check(int n, long int odd, long int count)
+int check(int n, int odd, int count)
 {
-long int sum, _sqrt;
+int sum;
+long  _sqrt;
 if (n < 0)
 {
 return (-1);
@@ -21,15 +22,14 @@ if (n == 0)
 return (0);
 }
 count += 1;
-sum = check(n - odd, 2 * count - 1, count);
+sum = check(n - odd, (2L * count) -1, count);
 if (sum < 0)
 {
 return (-1);
 }
-_sqrt = (check(n - odd, 2 * count - 1, count) + 1);
+_sqrt = (check(n - odd, 2L * count - 1, count) + 1);
 return (_sqrt);
 }
-
 /**
  * _sqrt_recursion - returns the sqrt of n.
  *
@@ -39,11 +39,12 @@ return (_sqrt);
  */
 int _sqrt_recursion(int n)
 {
-int c;
+int c, odd;
 c = 1;
-if (n < 0)
+odd = 1;
+if (n == 1)
 {
-return (-1);
+return (1);
 }
-return (check(n, c, c));
+return (check(n, odd, c));
 }
