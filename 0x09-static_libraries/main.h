5cbaeaadb28332a9bee483dbce592b9b731d46ef
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-03 19:32:55 -0700

    update

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
-#include <unistd.h>
-#include <stdio.h>
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 
 int _islower(int c);
@@ -40,3 +39,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+#endif /* !MAIN_H/
