23b33bfe9a0daad248c431e85966bf0d1ecd62c2
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-05 05:28:30 -0700

    added program to prints num of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the num of args.
+ *
+ * @argc: the count of the program command
+ * @argv: an arr of pointer to char
+ *
+ * Return: nothing.
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
