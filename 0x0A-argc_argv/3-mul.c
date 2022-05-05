8f29da6969a715512a125392bf489835bac02e12
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-05 06:27:20 -0700

    added program to multply 2 nums

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the product of two nums.
+ *
+ * @argc: the count of the program command
+ * @argv: an arr of pointer to char
+ *
+ * Return: 1 on success.
+ */
+int main(int argc, char *argv[])
+{
+int mult = 1, i;
+if (argc > 2)
+{
+for (i = 1; i < argc; i++)
+{
+mult *= atoi(argv[i]);
+}
+printf("%d\n", mult);
+}
+else
+{
+printf("Error\n");
+}
+return (1);
+}
