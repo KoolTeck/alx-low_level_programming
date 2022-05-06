be5cb07ee7fd451f6bc92a30ca6e12d341afc879
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-06 03:52:08 -0700

    added duplicate function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string
+ *
+ * @str: the string to duplicate.
+ *
+ * Return: the ptr to the dup. or NULL
+ */
+char *_strdup(char *str)
+{
+int i, len;
+char *s;
+if (str == NULL)
+{
+return (NULL);
+}
+len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+s = malloc(len *sizeof(char));
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len; i++)
+{
+s[i] = str[i];
+}
+s[i] = '\0';
+return (s);
+}
