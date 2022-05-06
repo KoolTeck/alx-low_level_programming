ece01cec2325dcf23a3eae4e291b1e7ad568a34f
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-06 07:50:33 -0700

    updates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,40 @@
 #include <stdlib.h>
 
 /**
+ * *concat - concats two strs.
+ *
+ * @s1: the ist str.
+ * @s2: the second str.
+ * @size1: the size of str1.
+ * @size2: the size of str2.
+ *
+ * Return: a pointer to the str or null on error.
+ */
+char *concat(int size1, int size2, char *s1, char *s2)
+{
+int i, j;
+char *s;
+i = 0, j = 0;
+s = malloc(size1 + size2 + 1 * sizeof(char));
+if (s == NULL)
+{
+return (NULL);
+}
+while (i < size1)
+{
+s[i] = s1[i];
+i++;
+}
+while (j < size2)
+{
+s[size1] = s2[j];
+size1++;
+j++;
+}
+s[size1] = '\0';
+return (s);
+}
+/**
  * str_concat - concats two strs.
  *
  * @s1: the ist str.
@@ -12,11 +46,20 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int i, j, len1, len2;
+int len1, len2;
 char *s;
-if (s1 == NULL || s2 == NULL)
+
+if (s1 == NULL && s2 == NULL)
 {
-return ('\0');
+return ("\0");
+}
+if (s1 == NULL)
+{
+return (s2);
+}
+else if (s2 == NULL)
+{
+return (s1);
 }
 len1 = 0;
 len2 = 0;
@@ -28,20 +71,6 @@ while (s2[len2] != 0)
 {
 len2++;
 }
-s = malloc(len1 + len2 + 1 * sizeof(char));
-i = 0;
-while (i < len1)
-{
-s[i] = s1[i];
-i++;
-}
-j = 0;
-while (j < len2)
-{
-s[len1] = s2[j];
-len1++;
-j++;
-}
-s[len1] = '\0';
+s = concat(len1, len2, s1, s2);
 return (s);
 }
