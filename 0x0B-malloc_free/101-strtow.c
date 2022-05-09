b35bf10316571bf6f185f58a81ea5da7aa344b1e
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-09 02:41:27 -0700

    updated 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * filter_arr - fillters an arr of words on null.
+ * @arr: The arr to filter.
+ * @str_len: the length of the str
+ *
+ * Return: a pointer to the new arr;
+ */
+char **filter_arr(char **arr, int str_len)
+{
+int i, arr_len = 0;
+char *s;
+for (i = 0; i < str_len; i++)
+{
+s = arr[i];
+if (s[0] == 0 || s[0] == 1)
+{
+continue;
+}
+arr[arr_len] = s;
+arr_len++;
+}
+arr[arr_len] = '\0';
+return (arr);
+}
+
+/**
  * fill_buffer - fills buffer with arr of words.
  * @buffer: The buffer to fill
  * @str: the string to fill.
@@ -10,10 +35,10 @@
  */
 char **fill_buffer(char **buffer, char *str, int str_len)
 {
-int i, j, row_c;
+int i, j, count = 0, row_c;
 j = 0;
 row_c = 0;
-for (i = 0; i <= str_len; i++)
+for (i = 0; i < str_len; i++)
 {
 if (str[i] == ' ' || str[i] == '\0')
 {
@@ -25,10 +50,13 @@ else
 {
 buffer[row_c][j] = str[i];
 j++;
+count++;
 }
-buffer[row_c][j] = '\0';
 }
-buffer[row_c][j] = '\0';
+if (count == 0)
+{
+return (NULL);
+}
 return (buffer);
 }
 
@@ -83,5 +111,10 @@ return (NULL);
 }
 }
 stw = fill_buffer(stw, str, str_len);
+if (stw == NULL)
+{
+return (NULL);
+}
+stw = filter_arr(stw, str_len);
 return (stw);
 }
