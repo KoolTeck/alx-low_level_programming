a06093be4b5c3a302bd499f43db17a1dac42517e
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-09 21:21:18 -0700

    added calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates mem for an arr.
+ *
+ * @nmemb: the size of the ar.
+ * @size: the size of element type.
+ *
+ * Return: a pointer to the mem.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *ptr;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < nmemb; i++)
+{
+ptr[i] = 0;
+}
+return (ptr);
+}
