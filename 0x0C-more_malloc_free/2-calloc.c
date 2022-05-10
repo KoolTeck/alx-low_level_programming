e3c48ff5fa066efa32b214fd54fd37df6361edce
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-09 21:32:41 -0700

    added calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
-char *ptr;
+void *ptr;
+char *arr;
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
@@ -22,9 +23,10 @@ if (ptr == NULL)
 {
 return (NULL);
 }
+arr = ptr;
 for (i = 0; i < nmemb; i++)
 {
-ptr[i] = 0;
+arr[i] = 0;
 }
-return (ptr);
+return (arr);
 }
