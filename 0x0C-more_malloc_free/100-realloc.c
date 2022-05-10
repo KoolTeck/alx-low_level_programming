dc3662ceac2a6afa6f437458d014b9fb8c7f9ddb
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-10 10:46:57 -0700

    added realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _realloc - reallocates a memory block
+ *
+ * @ptr: the pointer to the prev mem. block
+ * @old_size: the size in byte of the prev block
+ * @new_size: the new byte to be added.
+ *
+ * Return: a pointer to the new mem block or null
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *new_ptr;
+new_ptr = ptr;
+if (new_size > old_size)
+{
+new_ptr = malloc(old_size + new_size * sizeof(int));
+}
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (ptr == NULL)
+{
+new_ptr = malloc(new_size * sizeof(int));
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+free(ptr);
+return (new_ptr);
+}
