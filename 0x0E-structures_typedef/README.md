1b7514f07ca7ec404dd1adca9aedd32621dba0ba
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-11 14:58:49 -0700

    added dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# 0x0E-structures_typedef
+
+Containes solution to task on structures and typedefs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data for a dog breed
+ * @name: name of the dog
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* DOG_H */
