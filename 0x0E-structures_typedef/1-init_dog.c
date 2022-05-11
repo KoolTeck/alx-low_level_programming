a2e1459d5a2a569d2744634b47b66b78d9699320
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-11 16:15:52 -0700

    added init dog var

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initializes the var of type struct dog
+ *
+ * @d: the address of the struct to initialize
+ * @name: the name of the dog
+ * @age: the dogs age
+ * @owner: the dogs owner
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
