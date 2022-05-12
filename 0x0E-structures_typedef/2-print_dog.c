f2c062138405f33c10362a71f3eb25c9eed64ce2
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-11 17:23:25 -0700

    updates printdog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,14 @@ else
 {
 printf("Name: %s\n", d->name);
 }
+if (d->age <= 0)
+{
+printf("age: (nil)\n");
+}
+else
+{
 printf("Age: %f\n", d->age);
+}
 if (d->owner == NULL)
 {
 printf("owner: (nil)\n");
