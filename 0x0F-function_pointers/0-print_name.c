531f58ae38d5c159accb3246839ad0c8e6502f55
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-13 06:37:05 -0700

    added func ti print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ *
+ * @name: the name to print
+ * @f: function pointer to callback
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+# 0x0F-function_pointers
+
+Solutions to tasks on function pointers
