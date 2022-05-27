b689d08f0e77d2b0b199f4dd7399a0619ea2f052
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-27 01:01:43 -0700

    added function freeing list setting header to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees element of list listint_t
+ * @head: pointer to head element
+ *
+ * Return: Returns nothing
+ *
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp, *next_node;
+temp = *head;
+next_node = *head;
+
+if (temp != NULL)
+{
+while (temp != NULL)
+{
+temp = temp->next;
+free(next_node);
+next_node = temp;
+}
+*head = NULL;
+}
+}
