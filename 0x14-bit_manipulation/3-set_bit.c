31d44d8a891fcf6fd2db8ac00736330602e9eea9
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-06-01 05:09:36 -0700

    updates

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,43 @@
 #include "main.h"
 
 /**
+ * rev_string - reverse a string
+ *
+ * @s: pointer to the char array.
+ *
+ * Return: nothing.
+ */
+void rev_string(char *s)
+{
+int len;
+int i, j, k, tmp;
+
+i = 0;
+k = 0;
+if (s[i] != 0)
+{
+while (s[k] != 0)
+{
+len = k;
+k++;
+}
+j = len;
+while (i < j)
+{
+tmp = s[i];
+s[i] = s[j];
+s[j] = tmp;
+i++;
+j--;
+}
+}
+else
+{
+s[i] = s[i];
+}
+}
+
+/**
  * str_len - returns the lenght of a string
  *
  * @b: the str to check
