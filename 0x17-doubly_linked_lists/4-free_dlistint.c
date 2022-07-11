bc919103a8be730b435dbd7bb3219f7c915e0d34
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-07-11 02:35:21 -0700

    added freeing doubly linkd list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
