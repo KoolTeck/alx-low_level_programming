efa18d76f10530b66e60b1611c3842760960f9f8
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-11-02 12:22:03 -0700

    added jump search algirithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *arr, int n, int start);
+int jump_search(int *array, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
