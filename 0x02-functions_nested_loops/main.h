e8f862713aaab45d0b7fae502664549d2f36aa42
Adetunji, Adebowale <bowaleadetunji@gmail.com>
2023-01-30 15:23:28 +0100

    updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,42 +1,14 @@
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- */
-
 int _putchar(char c);
-
-/**
- * print_alphabet - prints small alphas to stdout.
- *
- */
 void print_alphabet(void);
-
-/**
- * prints lowercase alpha 10 times
- */
 void print_alphabet_x10(void);
-
-/**
- * returns 1 for true and 0 otherwise
- */
-
 int _islower(int c);
-
 int _isalpha(int c);
-
 int print_sign(int n);
-
-/**
- * returns + or - for true and 0 otherwise
- */
-int _abs(int num);
-
-int print_last_digit(int num);
-
+int _abs(int r);
+int print_last_digit(int r);
 void jack_bauer(void);
-
-int add(int, int);
-
+void times_table(void);
+int add(int a, int b);
 void print_to_98(int n);
+void print_times_table(int n);
+void putformat(int);
